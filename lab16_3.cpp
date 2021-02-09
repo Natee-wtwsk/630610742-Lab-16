b99030595c4ba294f2528cd9c175a830365324f6
Natee <gg977120@gmail.com>
2021-02-09 16:58:02 +0700

    Finish String Creator.

--- lab16_3.cpp
+++ lab16_3.cpp
@@ -22,3 +22,24 @@ int main(){
 }
 
 //Write Definition of randomVector() showVector() and dotProduct() here
+
+vector<int> randomVector(int N){	
+    vector<int> v;
+    for(int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> N){
+	cout << "[";
+	for(unsigned int i = 0; i < N.size(); i++){
+		cout << N[i];
+		if(i == N.size()-1) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> A,vector<int> B){
+	int sum = 0;
+	for(unsigned int i = 0; i < A.size(); i++) sum += A[i]*B[i];	
+	return sum;
+}
